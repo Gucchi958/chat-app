a2b738be66b4b46b9a86b7d728bd95623e59fe16
Gucchi958 <81725643+Gucchi958@users.noreply.github.com>
2021-04-06 17:49:25 +0900

    各モデルのテスト

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -19,7 +19,7 @@ class RoomsController < ApplicationController
   def destory
     room = Room.find(params[:id])
     room.destory
-    redirect_to root_path
+    redirect_to root_path                                                 
   end
 
   private
